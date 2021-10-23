b2a2b083d83bf1a2e46e8cc10d58a24d84e5236b
tikh <tikhtmp@mail.ru>
2021-10-23 14:26:53 +0300

    Cleared from trash

--- src/main/java/by/karas/onlinestore/repos/ProductRepository.java
+++ src/main/java/by/karas/onlinestore/repos/ProductRepository.java
@@ -4,9 +4,4 @@ import by.karas.onlinestore.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-//    @Query("SELECT product FROM  Product product order by product.name")
-//    //@Query("SELECT ptype FROM PetType ptype ORDER BY ptype.name")
-//    @Transactional(readOnly = true)
-//    List<Product> findProducts();
-//    List<Product> findAllByName(String name);
 }
