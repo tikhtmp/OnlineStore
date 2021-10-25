9fedc415ecdc4cbd4519c6dff1ca9056f38dbd55
tikh <tikhtmp@mail.ru>
2021-10-25 20:45:32 +0300

    Login added.

--- src/main/java/by/karas/onlinestore/controller/ProductController.java
+++ src/main/java/by/karas/onlinestore/controller/ProductController.java
@@ -4,8 +4,10 @@ import by.karas.onlinestore.domain.Product;
 import by.karas.onlinestore.repos.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -21,29 +23,36 @@ public class ProductController {
     }
 
     @GetMapping("/home")
-    public String home(/*@RequestParam ("login") String login,*/ Map<String, Object> model){
-        //model.put("login", login);
-        Iterable<Product> products;
-        products = productRepository.findAll();
-        model.put("products", products);
-        Product product = new Product();
-        model.put("product", product);
+    public String home(@RequestParam (value = "login", defaultValue = "visitor") String login, Model /*Map<String, Object>*/ model){
+        model.addAttribute("login", login);
+        System.out.println("login = " + login);
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
         return "home_page";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam ("login") String login, Model model){
+        model.addAttribute("login", login);
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "home_page";
+    }
+
+
 
     @PostMapping("/products")
     public String products(@RequestParam ("name") String name,
                            @RequestParam ("price") Double price,
-                           Map<String, Object> model){
-        //Product newProduct = new Product(null, name, price);
-        Product newProduct = new Product(name, price);
-        productRepository.save(newProduct);
-        Iterable<Product> products;
-        products = productRepository.findAll();
+                           Model model){
+//                           Map<String, Object> model){
+//        Product newProduct = new Product(name, price);
+//        productRepository.save(newProduct);
+        Iterable<Product> products = productRepository.findAll();
 //        List<Product> productList = new ArrayList<>();
 //        productList.add(newProduct);
         //model.put("products", products);
-        model.put("products", products);
+        model.addAttribute("products", products);
         return "products_page";
     }
 }
